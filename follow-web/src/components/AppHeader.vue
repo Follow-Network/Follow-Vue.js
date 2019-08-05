3421f10c31207716c66c804d12459f66f2ceeb6f
dimaty26 <dimaty26rus@mail.ru>
2019-08-05 15:39:12 +0300

    Add newpage

---
+++ follow-web/src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app>
+        <v-toolbar app dark class="primary">
+            <v-toolbar-title v-text="'Follow'"></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn color>Button</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        computed: {
+            menuItems() {
+                return [
+                    {
+                        icon: 'visibility',
+                        title: 'Sign Up',
+                        route: '/login'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ follow-web/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+};
+</script>
